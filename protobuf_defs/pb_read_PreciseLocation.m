3f71f6f881405a6f8ed85d983bd66cc8f0c14ffb
ericeckstrand <eric.eckstrand@gmail.com>
2014-10-28 14:35:01 -0700

    removed Substrate type. added PrecisionLocation, VideoSubstrate, and ImageSubstrate files.

---
+++ protobuf_defs/pb_read_PreciseLocation.m
@@ -0,0 +1,30 @@
+function [precise_location] = pb_read_PreciseLocation(buffer, buffer_start, buffer_end)
+%pb_read_PreciseLocation Reads the protobuf message PreciseLocation.
+%   function [precise_location] = pb_read_PreciseLocation(buffer, buffer_start, buffer_end)
+%
+%   INPUTS:
+%     buffer       : a buffer of uint8's to parse
+%     buffer_start : optional starting index to consider of the buffer
+%                    defaults to 1
+%     buffer_end   : optional ending index to consider of the buffer
+%                    defaults to length(buffer)
+%
+%   MEMBERS:
+%     x              : optional single, defaults to single(0).
+%     y              : optional single, defaults to single(0).
+%
+%   See also pb_read_MatlabBridgeMsg, pb_read_Model, pb_read_Properties, pb_read_Result, pb_read_ResultList, pb_read_ResultSet, pb_read_DirectoryPath, pb_read_FilePath, pb_read_ImageSubstrate, pb_read_VideoSubstrate, pb_read_FramePathPair, pb_read_Semantics, pb_read_LabelProperties, pb_read_Label, pb_read_Labelable, pb_read_LabeledTrack, pb_read_FrameLocationList, pb_read_FrameLocation, pb_read_VideoSeekTime, pb_read_Point2D, pb_read_LabelableList, pb_read_Purpose, pb_read_PurposedLabelableSeq, pb_read_PurposedListSequence, pb_read_RunSet.
+
+  if (nargin < 1)
+    buffer = uint8([]);
+  end
+  if (nargin < 2)
+    buffer_start = 1;
+  end
+  if (nargin < 3)
+    buffer_end = length(buffer);
+  end
+
+  descriptor = pb_descriptor_PreciseLocation();
+  precise_location = pblib_generic_parse_from_string(buffer, descriptor, buffer_start, buffer_end);
+  precise_location.descriptor_function = @pb_descriptor_PreciseLocation;
